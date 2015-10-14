02d8f9611b3780b172efd07441808dc018092d7f
alexpashevich <alexpashevich@github.com>
2015-10-14 12:49:27 +0200

    everything is done

--- convolution.py
+++ convolution.py
@@ -1,15 +1,12 @@
 __author__ = 'alexpashevich'
 
-import numpy as np
-import matplotlib.pyplot as plt
-from linear_system import show_plot
+from linear_system import show_arr
 
 
 def myConvolution1D(x, h):
     # function assumes that that len(h) < len(x)
     conv = []
     for k in range(len(x) + len(h) - 1):
-        print k
         val = 0
         if k < len(h):
             for i in range(k + 1):
@@ -23,12 +20,26 @@ def myConvolution1D(x, h):
         conv.append(val)
     return conv
 
+def myMovingAverage(x, M):
+    if M % 2 == 0:
+        print("Error, M is not odd!")
+        return []
+    y = []
+    h = [1. / M] * M
+    z = myConvolution1D(x, h)
+    for i in range(len(z) - M + 1):
+        y.append(z[i + (M - 1) / 2])
+    return y
 
-x = [1,0,2,3,2,1,-1,-2,-1,0,2,3,3,2,1,1]
-h = [2,2,-1,-1,3]
-conv = myConvolution1D(x, h)
+if __name__ == "__main__":
+    x = [1,0,2,3,2,1,-1,-2,-1,0,2,3,3,2,1,1]
+    h = [2,2,-1,-1,3]
+    conv = myConvolution1D(x, h)
 
-# question 2.2
-show_plot(range(len(x)), x, 'Input', 'k', 'x')
-show_plot(range(len(h)), h, 'Kernel', 'k', 'h')
-show_plot(range(len(conv)), conv, 'Convolution', 'k', 'x*h')
+
+    # question 2.2
+    show_arr(x, 'Input', 'k', 'x')
+    show_arr(h, 'Kernel', 'k', 'h')
+    show_arr(conv, 'Convolution', 'k', 'x*h')
+
+    # question 2.5, 2.6 and 2.7 are in linear_system.py
--- linear_system.py
+++ linear_system.py
@@ -3,7 +3,6 @@ __author__ = 'alexpashevich'
 import numpy as np
 import matplotlib.pyplot as plt
 
-
 def create_x1(size):
     return [np.sin(2 * np.pi * k / 100) for k in range(size)]
 
@@ -37,6 +36,9 @@ def show_plot(x, y, title="Plot", x_name="x", y_name="y"):
     plt.ylabel(y_name)
     plt.show()
 
+def show_arr(y, title="Plot", x_name="x", y_name="y"):
+    show_plot(range(len(y)), y, title, x_name, y_name)
+
 
 def singleLowPassFilter(x):
     y = [0.]
@@ -58,28 +60,34 @@ if __name__ == "__main__":
     sum_of_3 = [x1[i] + x2[i] + x3[i] for i in range(size)]
     sum_of_filtered_3 = [x1_filtered[i] + x2_filtered[i] + x3_filtered[i] for i in range(size)]
 
-    x_noise = map(sum, zip(sum_of_3, x4))
-    x_noise_filtered = singleLowPassFilter(x_noise)
+    x_noisy = map(sum, zip(sum_of_3, x4))
+    x_noisy_filtered = singleLowPassFilter(x_noisy)
 
     # question 1.4
-    show_plot(range(size), sum_of_3, "Sum of three signals", "k", "x1 + x2 + x3")
-    show_plot(range(size), map(sum, zip(sum_of_3, x4)), "Sum of four signals", "k", "x1 + x2 + x3 + x4")
+    show_arr(sum_of_3, "Sum of three signals", "k", "x1 + x2 + x3")
+    show_arr(map(sum, zip(sum_of_3, x4)), "Sum of four signals", "k", "x1 + x2 + x3 + x4")
 
     # question 1.6
-    show_plot(range(size), x1, "x1", "k", "x1")
-    show_plot(range(size), x1_filtered, "x1 filtered", "k", "S(x1)")
-    show_plot(range(size), x2, "x2", "k", "x2")
-    show_plot(range(size), x2_filtered, "x2 filtered", "k", "S(x2)")
-    show_plot(range(size), x3, "x3", "k", "x3")
-    show_plot(range(size), x3_filtered, "x3 filtered", "k", "S(x3)")
+    show_arr(x1, "x1", "k", "x1")
+    show_arr(x1_filtered, "x1 filtered", "k", "S(x1)")
+    show_arr(x2, "x2", "k", "x2")
+    show_arr(x2_filtered, "x2 filtered", "k", "S(x2)")
+    show_arr(x3, "x3", "k", "x3")
+    show_arr(x3_filtered, "x3 filtered", "k", "S(x3)")
 
     # question 1.7
-    show_plot(range(size), sum_of_3, "Sum of three signals", "k", "x1 + x2 + x3")
-    show_plot(range(size), singleLowPassFilter(sum_of_3), "Sum of three signals filtered", "k", "S(x1+x2+x3)")
+    show_arr(sum_of_3, "Sum of three signals", "k", "x1 + x2 + x3")
+    show_arr(singleLowPassFilter(sum_of_3), "Sum of three signals filtered", "k", "S(x1+x2+x3)")
 
     # question 1.8
-    show_plot(range(size), sum_of_filtered_3, "Sum of three filters", "k", "S(x1)+S(x2)+S(x3)")
+    show_arr(sum_of_filtered_3, "Sum of three filters", "k", "S(x1)+S(x2)+S(x3)")
 
     # question 1.10
-    show_plot(range(size), x_noise, "x_noise", "k", "x_noise")
-    show_plot(range(size), x_noise_filtered, "x_noise filtered", "k", "S(x_noise)")
+    show_arr(x_noisy, "x_noisy", "k", "x_noisy")
+    show_arr(x_noisy_filtered, "x_noisy filtered", "k", "S(x_noisy)")
+
+    # question 2.5, 2.6, 2.7
+    from convolution import myMovingAverage
+    x_noisy_mov_average = myMovingAverage(x_noisy, 51)
+    show_arr(x_noisy_filtered, 'Moving average on gaussian noise', 'k', 'y')
+    show_arr(x_noisy_filtered, "x_noisy filtered", "k", "S(x_noisy)")
